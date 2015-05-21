dc8fc5e271a6fb3d3a385b6c003d03b1dd7c9132
Flavius12 <info@evolsoft.tk>
2015-05-21 20:03:28 +0200

    Create ServerAuthPasswordChangeEvent.php

---
+++ ServerAuth/src/ServerAuth/Events/ServerAuthPasswordChangeEvent.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * ServerAuth (v1.00) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: http://www.evolsoft.tk
+ * Date: 10/05/2015 02:22 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ServerAuth/blob/master/LICENSE)
+ */
+
+namespace ServerAuth\Events;
+
+use pocketmine\event\plugin\PluginEvent;
+use pocketmine\Player;
+
+class ServerAuthPasswordChangeEvent extends PluginEvent {
+
+	public static $handlerList = null;
+
+	/** @var Player $player */
+	private $player;
+	
+	/** @var $password */
+	private $password;
+
+	/**
+	 * @param Player $player
+	 * @param $password
+	 */
+	public function __construct(Player $player, $password){
+		$this->player = $player;
+		$this->password = $password;
+	}
+
+	/**
+	 * Get event player
+	 *
+	 * @return Player
+	 */
+	public function getPlayer(){
+		return $this->player;
+	}
+	
+	/**
+	 * Get the new password
+	 * 
+	 * @return $password
+	 */
+	public function getPassword(){
+		return $this->password;
+	}
+}
