73ccd515eba000b336d993f787bad08a22adc8b3
Flavius12 <info@evolsoft.tk>
2015-05-21 20:02:45 +0200

    Create ServerAuthAuthenticateEvent.php

---
+++ ServerAuth/src/ServerAuth/Events/ServerAuthAuthenticateEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * ServerAuth (v1.00) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: http://www.evolsoft.tk
+ * Date: 10/05/2015 02:17 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ServerAuth/blob/master/LICENSE)
+ */
+
+namespace ServerAuth\Events;
+
+use pocketmine\event\plugin\PluginEvent;
+use pocketmine\Player;
+
+class ServerAuthAuthenticateEvent extends PluginEvent {
+
+	public static $handlerList = null;
+
+	/** @var Player $player */
+	private $player;
+
+	/**
+	 * @param Player $player
+	 */
+	public function __construct(Player $player){
+		$this->player = $player;
+	}
+
+	/**
+	 * Get event player
+	 *
+	 * @return Player
+	 */
+	public function getPlayer(){
+		return $this->player;
+	}
+}
