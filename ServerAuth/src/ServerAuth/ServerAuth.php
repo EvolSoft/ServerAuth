fbb740304a83d0ca7f27515d8fa95ba40e1cbf78
TheDiamondYT1 <luke.wyatt21@gmail.com>
2017-03-18 14:07:25 +0000

    revert

--- ServerAuth/src/ServerAuth/ServerAuth.php
+++ ServerAuth/src/ServerAuth/ServerAuth.php
@@ -133,7 +133,31 @@ class ServerAuth extends PluginBase {
      * @return string The translated message
      */
     public function translateColors($symbol, $message){
-    	return str_replace($symbol, TextFormat::ESCAPE, $message);
+    	$message = str_replace($symbol."0", TextFormat::BLACK, $message);
+    	$message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
+    	$message = str_replace($symbol."2", TextFormat::DARK_GREEN, $message);
+    	$message = str_replace($symbol."3", TextFormat::DARK_AQUA, $message);
+    	$message = str_replace($symbol."4", TextFormat::DARK_RED, $message);
+    	$message = str_replace($symbol."5", TextFormat::DARK_PURPLE, $message);
+    	$message = str_replace($symbol."6", TextFormat::GOLD, $message);
+    	$message = str_replace($symbol."7", TextFormat::GRAY, $message);
+    	$message = str_replace($symbol."8", TextFormat::DARK_GRAY, $message);
+    	$message = str_replace($symbol."9", TextFormat::BLUE, $message);
+    	$message = str_replace($symbol."a", TextFormat::GREEN, $message);
+    	$message = str_replace($symbol."b", TextFormat::AQUA, $message);
+    	$message = str_replace($symbol."c", TextFormat::RED, $message);
+    	$message = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $message);
+    	$message = str_replace($symbol."e", TextFormat::YELLOW, $message);
+    	$message = str_replace($symbol."f", TextFormat::WHITE, $message);
+    
+    	$message = str_replace($symbol."k", TextFormat::OBFUSCATED, $message);
+    	$message = str_replace($symbol."l", TextFormat::BOLD, $message);
+    	$message = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $message);
+    	$message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
+    	$message = str_replace($symbol."o", TextFormat::ITALIC, $message);
+    	$message = str_replace($symbol."r", TextFormat::RESET, $message);
+    
+    	return $message;
     }
     
     /**
